45f2506d005f82d19fc38deb78959ef4360e3b84
leukosaraphs <gbalaouras@yahoo.com>
2019-12-15 15:22:25 +0200

    Demo commit

--- huffDec.m
+++ huffDec.m
@@ -7,8 +7,8 @@ for i = 1:totalSize
     currChar = strcat(char(currChar), huffStream(i));
     index = find(DC_Huff_L(:,3) == string(currChar));
     if index                                            % Found the category
-        Category = double(DC_Huff_L(index,1));
-        currChar = huffStream(i+1:i+Category);          % The additional Bits
+        Category = hex2dec(DC_Huff_L(index,1));       % Get the addtional bit length
+        currChar = huffStream(i+1:i+Category);        % The additional Bits
         dcAdditionalValue = getDecimal(currChar);
         runSymbols(1,:) = [0 dcAdditionalValue];
         endOfDC = i+Category;
@@ -19,12 +19,18 @@ end
 currChar = []; j = endOfDC + 1;
 while ( j < totalSize)
     currChar = strcat(char(currChar), huffStream(j));
+    %disp(currChar);
     index = find(AC_Huff_L(:,4) == string(currChar));
-    if index
-        Category = double(AC_Huff_L(index,2));
+    if index == 1 | index == 152                         % EOB OR [15 0]
+        runSymbols = [runSymbols; [AC_Huff_L(index,1) 0]];
+        currChar = [];
+    elseif index
+        Category = hex2dec(AC_Huff_L(index,2));
+        disp(currChar);
         currChar = huffStream(j+1:j+Category);          % The additional Bits
+        disp(currChar);
         acAdditionalValue = getDecimal(currChar);
-        runSymbols = [runSymbols; [double(AC_Huff_L(index,1)) acAdditionalValue]];
+        runSymbols = [runSymbols; [AC_Huff_L(index,1) acAdditionalValue]];
         currChar = [];
         j = j + Category;                               % Go on to the next AC coeficient.
     end
